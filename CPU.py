de5a1e0751eb54e8f33bde878a5b46e406c15ae1
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2022-11-24 07:31:57 -0500

    Added memory read method for memory class to return data by memory address

--- CPU.py
+++ CPU.py
@@ -2,9 +2,10 @@ import os, sys
 
 class Memory:
     # Constructs memory to be used. Data for memory block is stored utilizing an index
-    def __init__(self):
+    def __init__(self, name):
         self.blocks = None
         self.data = None
+        self.name = name
         print(self.data)
     
     # Stores values within memory to be used
@@ -16,10 +17,16 @@ class Memory:
         self.blocks = len(data_to_store)
         self.data = data_to_store
         print(self.data)
+    
+    def memory_read(self, address):
+        for data in self.data:
+            if data[0] == address:
+                return data[1]
+        print(f"Memory address {address} was not found")
 
 class Cache(Memory):
     def __init__(self, main_memory):
-        super().__init__()
+        super().__init__(name="cache")
         self.main_memory = main_memory
         self.blocks = 4
         self.data = ["" for x in range(0, self.blocks)]
@@ -31,7 +38,7 @@ class CPU:
         self.instruction_address_register = 0
         self.instruction_register = None
         self.cache_state = 1
-        self.main_memory = Memory(self.main_memory)
+        self.main_memory = Memory(self.main_memory, name="main memory")
         self.cache = Cache()
         self.halt = 0
 
@@ -39,9 +46,9 @@ class CPU:
     
     def fetch_instructions(self):
         if self.cache_state == 0:
-            self.instruction_register = self.main_memory.read(self.instruction_address_register)
+            self.instruction_register = self.main_memory.memory_read(self.instruction_address_register)
         else:
-            self.instruction_register = self.cache.read(self.instruction_address_register)
+            self.instruction_register = self.cache.cache_read(self.instruction_address_register)
         
     
 
